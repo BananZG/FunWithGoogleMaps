58e5afdc5fb488b1230f12d8984e9f40859c24a2
Gan Jun Jie <junjie.gan@sicmsb.com>
2019-01-22 18:47:15 +0800

    Updated dummy markers logic v0.1

--- FunWithGoogleMaps/MapViewController.swift
+++ FunWithGoogleMaps/MapViewController.swift
@@ -18,6 +18,11 @@ struct SingaporeLocation {
 
 class LocationUtils : NSObject {
     
+    let minLatitude = -85.0
+    let maxLatitude = 85.0
+    let minLongitude = -180.0
+    let maxLongitude = 180.0
+    
     func getInitCameraLocation() -> GMSCameraPosition {
         let location = SingaporeLocation()
         return GMSCameraPosition.camera(withLatitude: location.latitude, longitude: location.longitude, zoom: location.zoom)
@@ -28,9 +33,10 @@ class LocationUtils : NSObject {
     }
 }
 
-class MapViewController: UIViewController, GMSMapViewDelegate {
+class MapViewController: UIViewController, GMSMapViewDelegate, CLLocationManagerDelegate {
     let locationUtils = LocationUtils()
     var mapView: GMSMapView?
+    var locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,8 +48,25 @@ class MapViewController: UIViewController, GMSMapViewDelegate {
         mapView.settings.myLocationButton = true
         mapView.delegate = self
         
+        self.locationManager.delegate = self
+        self.locationManager.startUpdatingLocation()
+        
         view = mapView
     }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = locations.last
+        
+        let camera = GMSCameraPosition.camera(withLatitude: (location?.coordinate.latitude)!, longitude: (location?.coordinate.longitude)!, zoom: 17.0)
+        
+        self.mapView?.animate(to: camera)
+        
+        //Finally stop updating location otherwise it will come again and again in this delegate
+        self.locationManager.stopUpdatingLocation()
+        
+    }
+
     
     var allMarkers: [GMSMarker] = []
     
@@ -57,26 +80,31 @@ class MapViewController: UIViewController, GMSMapViewDelegate {
         // mapView.clear()
         
         // 2. create marker based on current view (dummy markers a.t.m.)
-        let numberOfCases = 100
+        let numberOfMarkers = 100
         let topLeft = mapView.projection.visibleRegion().farLeft
-        let BottomLeft = mapView.projection.visibleRegion().nearLeft
+        let bottomLeft = mapView.projection.visibleRegion().nearLeft
         let topRight = mapView.projection.visibleRegion().farRight
+        let bottomRight = mapView.projection.visibleRegion().nearRight
         
-        let lARange: Range<Double>
-        if topLeft.latitude > BottomLeft.latitude {
-            lARange = BottomLeft.latitude ..< topLeft.latitude
-        } else {
-            lARange = topLeft.latitude ..< BottomLeft.latitude
-        }
+        print("all latitude", topLeft.latitude, topRight.latitude, bottomLeft.latitude, bottomRight.latitude)
+        print("all longitude", topLeft.longitude, topRight.longitude, bottomLeft.longitude, bottomRight.longitude)
+        
+        let lARange: Range<Double> = min(topLeft.latitude, topRight.latitude, bottomLeft.latitude, bottomRight.latitude) ..< max(topLeft.latitude, topRight.latitude, bottomLeft.latitude, bottomRight.latitude)
         
         let lORange: Range<Double>
-        if topRight.longitude > BottomLeft.longitude {
-            lORange = BottomLeft.longitude ..< topRight.longitude
-        } else {
-            lORange = topRight.longitude ..< BottomLeft.longitude
-        }
+        let shouldTurnOver = checkVariableHasPositiveAndNegativeValuesMixed(topLeft.longitude, topRight.longitude, bottomLeft.longitude, bottomRight.longitude)
+        // TODO: Update logic in next commit
+//        if shouldTurnOver {
+//            if Int.random(in: 0 ..< 2) == 0 {
+//                lORange = locationUtils.minLongitude ..< min(topLeft.longitude, topRight.longitude, bottomLeft.longitude, bottomRight.longitude)
+//            } else {
+//                lORange = max(topLeft.longitude, topRight.longitude, bottomLeft.longitude, bottomRight.longitude) ..< locationUtils.maxLongitude
+//            }
+//        } else {
+            lORange = min(topLeft.longitude, topRight.longitude, bottomLeft.longitude, bottomRight.longitude) ..< max(topLeft.longitude, topRight.longitude, bottomLeft.longitude, bottomRight.longitude)
+//        }
         
-        for _ in 0 ..< numberOfCases {
+        for _ in 0 ..< numberOfMarkers {
             
             // Creates markers in the map.
             let marker = GMSMarker()
@@ -90,6 +118,22 @@ class MapViewController: UIViewController, GMSMapViewDelegate {
         // 3. Magic!
         
         
+    }
+    
+    func checkVariableHasPositiveAndNegativeValuesMixed(_ param: Double...) -> Bool {
+        guard param.count > 2 else {
+            return false
+        }
+        let checkPositive = param.first! < 0.0
+        for i in 1 ..< param.count {
+            let e = param[i]
+            if checkPositive {
+                if e >= 0 { return true }
+            } else {
+                if e < 0 { return true }
+            }
+        }
+        return false
     }
 }
 
